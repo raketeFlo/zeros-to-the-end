1d06fd41bd620eb4019b88f6512b7131f135132f
Aleksey Mironenko <mironenkoalexey@gmail.com>
2019-08-01 19:43:20 +0200

    My solution

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 
 
-const moveZeros = () => {
-  // add your code here
+const moveZeros = (srcArr) => {
+  if (!Array.isArray(srcArr)) return [];
+  const nonZeros = srcArr.filter(item => item !== 0);
+  const zeros = Array(srcArr.length - nonZeros.length).fill(0);
+  return nonZeros.concat(zeros);
 };
 
 module.exports = moveZeros;
